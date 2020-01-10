5ec8f832ec871aefa9d06a9bc51b4ba92bd68c3b
ama <shalikacomm@gmail.com>
2020-01-10 10:44:38 +0530

    web initial

---
+++ src/main/java/gym/com/GymFinalUser/GymFinalUserApplication.java
@@ -0,0 +1,13 @@
+package gym.com.GymFinalUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymFinalUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymFinalUserApplication.class, args);
+	}
+
+}
